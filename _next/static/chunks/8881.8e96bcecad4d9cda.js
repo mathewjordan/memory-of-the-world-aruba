1d184c3a47ef82934b08e10f2603dd96ac8dcbd3
mathewjordan <mat@northwestern.edu>
2025-04-24 15:55:38 +0000

    Deploying to gh-pages from @ mathewjordan/memory-of-the-world-aruba@8a5797fe563e89aeaf19d31cf3b219b5a7266390 🚀

--- _next/static/chunks/8881.8e96bcecad4d9cda.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[8881],{58881:function(i){i.exports=JSON.parse('{"collection":"https://iiif.archive.org/iiif/3/mow-aw/collection.json"}')}}]);
